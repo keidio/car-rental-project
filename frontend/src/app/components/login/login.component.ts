48a183bad47b977d6945280860c2ddb037585d39
Keidi Oras <keidioras@gmail.com>
2023-07-16 14:02:25 +0300

    Login component -  form

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {LoginService} from "../../services/login-service/login.service";
 import {Router} from "@angular/router";
 import {homePageUrl} from "../../models/links";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -13,6 +14,14 @@ export class LoginComponent {
   message! : string;
   isLoggedIn!: boolean;
   isLoggedOut! :boolean;
+
+  loginForm = new FormGroup({
+    login: new FormControl(''),
+    password: new FormControl('')
+  })
+
+  loginControl = this.loginForm.controls.login;
+  passwordControl = this.loginForm.controls.password;
 
   constructor(
     private loginService: LoginService,
@@ -36,7 +45,8 @@ export class LoginComponent {
     console.log('inside login method()');
     this.message = "Trying to login....";
     this.loginService
-      .login('me', 'you')
+      .login(this.loginForm.controls.login.value as string,
+        this.loginForm.controls.password.value as string)
       .subscribe(value => {
         this.updateStatus();
         this.router.navigateByUrl(homePageUrl)
