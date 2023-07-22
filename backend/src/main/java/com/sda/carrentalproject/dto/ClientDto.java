a17a9e3b62f9a462f00e7252b79a861e8b6e90c8
Keidi Oras <keidioras@gmail.com>
2023-07-22 11:16:38 +0300

    Beans validations added on create new client endpoint

--- backend/src/main/java/com/sda/carrentalproject/dto/ClientDto.java
+++ backend/src/main/java/com/sda/carrentalproject/dto/ClientDto.java
@@ -1,20 +1,39 @@
 package com.sda.carrentalproject.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 // TODO: add nulls and not nulls annotations
 @Builder
 public record ClientDto( Long id,
+                         @NotNull
+                         @Size(min = 2)
                          String name,
+
+                         @NotNull
+                         @Size(min = 2)
                          String surname,
+
+                         @NotNull
+                         @Size(min = 7)
                          String phone,
+
+                         @NotNull
+                         @Email
                          String email,
                          String address,
+
                          boolean hasDrivingLicence,
+
                          LocalDateTime registrationDateTime,
-                         LocalDate dateOfBirth) {
+
+                         @Past
+                         LocalDate dateOfBirth
+) {
 
 }
